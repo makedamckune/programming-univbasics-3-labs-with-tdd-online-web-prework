0150ebcbefec891c7c40090c47d2604b0aa58df9
Makeda P Mckune <makedamckune@gmail.com>
2019-09-29 22:00:40 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,17 @@
-# Add your variables here
+ 
+ puts "first_number"
+num1 = gets.chomp().to_f 
+puts "enter operator"
+op = gets.chomp()
+puts "second_number"
+num2 = gets.chomp().to_f 
+
+if op == "+"
+  puts (num1+num2)
+elsif op == "-"
+ puts (num1-num2)
+elsif op == "*"
+ puts (num1*num2)
+elsif op == "/"
+ puts (num1/num2)
+end 
