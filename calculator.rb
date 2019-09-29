5e6f4bf6076c4619e630d6102004e5ff84dd0299
Makeda P Mckune <makedamckune@gmail.com>
2019-09-29 21:53:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,11 @@
-
-  puts "first_number"
+ 
+ puts "first_number"
 num1 = gets.chomp().to_f 
 puts "enter operator"
 op = get.chomp()
+puts "second_number"
+num2 = gets.chomp().to_f 
 
+if op == + 
+  puts (num1+num2)
 end
